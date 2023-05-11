a1e0df929898512ca64e5052f03962342714dd31
AgustinMassieri <agmassieri00@gmail.com>
2023-05-11 09:12:36 -0300

    Refactor

--- src/components/Reserva.js
+++ src/components/Reserva.js
@@ -10,43 +10,47 @@ const Reserva = ({ parking }) => {
 
 
     const updateParking = () => {
-        console.log(reservation)
+        console.log('Reseva: ' + reservation);
 
-        if (reservation.parkingId !== '')
-        {
+        if (reservation.parkingId !== ''){
             parking.numberOfParkingSpacesAvailable -= 1;
-            fetch('http://localhost:3001/api/parkings/'+parking._id, {
-                method: 'PUT',
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": "Bearer " + localStorage.getItem('token')
-                },
-                body: JSON.stringify(parking)
-            }).then(function(response) {
-                if(response.status === 200){
-                    //window.location.href = "/reservations";
-                }
-                else{
-                    console.log("no funciona")
-                } 
-            });
+            if(parking.numberOfParkingSpacesAvailable >= 0){
+                fetch('http://localhost:3001/api/parkings/'+parking._id, {
+                    method: 'PUT',
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": "Bearer " + localStorage.getItem('token')
+                    },
+                    body: JSON.stringify(parking)
+                }).then(function(response) {
+                    if(response.status === 200){
+                        //window.location.href = "/reservations";
+                        console.log("Se actualizo el estacionamiento")
+                    }
+                    else{
+                        console.log("Hubo un error actualizando el estacionamiento")
+                    } 
+                });
 
-            fetch('http://localhost:3001/api/reservations/', {
-                method: 'POST',
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": "Bearer " + localStorage.getItem('token')
-                },
-                body: JSON.stringify(reservation)
-            }).then(function(response) {
-                if(response.status === 200){
-                    console.log("funciona")
-                }
-                else{
-                    console.log("no funciona")
-                } 
-            });
-        }
+                fetch('http://localhost:3001/api/reservations/', {
+                    method: 'POST',
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": "Bearer " + localStorage.getItem('token')
+                    },
+                    body: JSON.stringify(reservation)
+                }).then(function(response) {
+                    if(response.status === 200){
+                        console.log("Se creo la reserva")
+                    }
+                    else{
+                        console.log("Hubo un error creando la reserva")
+                    } 
+                });
+            }
+        } else{
+            console.log("No se puede reservar ya que no hay lugares disponibles");
+        }   
     }
 
     useEffect(() => {
--- src/components/ReservationsTable.js
+++ src/components/ReservationsTable.js
@@ -8,13 +8,17 @@ const ReservationsTable = ({ reservations }) => {
         <Table style={{fontSize: '15px'}}>
             <thead>
                 <tr>
+                    <th style={{paddingRight: '35px'}}>Id de Reserva</th>
                     <th style={{paddingRight: '35px'}}>Nombre</th>
+                    <th style={{paddingRight: '35px'}}>User Id</th>
                 </tr>
             </thead>
             <tbody>
                 {reservations.map((reservation) => (
-                        <tr key={reservation.id}>
+                    <tr key={reservation._id}>
+                        <td style={{paddingRight: '35px'}}>{reservation._id}</td>
                         <td style={{paddingRight: '35px'}}>{reservation.parkingName}</td>
+                        <td style={{paddingRight: '35px'}}>{reservation.userId}</td>
                     </tr>
                 ))}
             </tbody>
